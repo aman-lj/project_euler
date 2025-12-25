587dd2ecc7748a42d0ea0e067e89db1fe46ff06e
aman.ghodawala <aman.ghodawala@zobiwebsolutions.com>
2025-12-25 10:13:51 +0530

    first commit

---
+++ ProjectEuler/Problem5.cs
@@ -0,0 +1,25 @@
+﻿namespace ProjectEuler;
+
+public static class Problem5
+{
+    public static void Solve()
+    {
+        int i = 1;
+        while (true)
+        {
+            bool isDivisible = true;
+            for (int j = 1; j <= 20; j++)
+            {
+                if(i % j != 0)
+                    isDivisible = false;
+            }
+
+            if (isDivisible)
+            {
+                Console.WriteLine(i);
+                break;
+            }
+            i++;
+        }
+    }
+}
